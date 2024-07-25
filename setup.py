e7ddc38faf69760b04bcb1b3f7253141dbdc9cac
Hugo Hadfield <hadfield.hugo@gmail.com>
2024-07-25 18:36:58 +0100

    long description

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rgbdslam",
-    version="0.1",
+    version="0.0.1",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -32,4 +32,6 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
 )
